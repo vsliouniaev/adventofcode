ff6aa4e3f4efc36d808c046fc0c75ac780e36a7f
Vasily Sliouniaev <v.sliouniaev@gmail.com>
2018-12-05 20:46:13 +0000

    2018-05.1

---
+++ 2018-05.fsx
@@ -0,0 +1,18 @@
+let readFile (filePath:string) = seq {
+    use sr = new System.IO.StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.Read ()
+}
+let diff = int('a') - int('A')
+let stack =  new System.Collections.Generic.Stack<FSharp.Core.Option<int>>()
+stack.Push None
+
+let reactPolymer value = 
+    match stack.Peek() with
+    | Some v when (abs(v - value)).Equals(diff) -> stack.Pop() |> ignore
+    | _ -> stack.Push(Some value)
+
+readFile "2018-05"
+|> Seq.iter reactPolymer
+
+stack.Count - 1
