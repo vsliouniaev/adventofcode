63e5b37d769797555fffdf6ffc8c200972f6d6ac
Vasily Sliouniaev <v.sliouniaev@gmail.com>
2018-12-05 01:03:58 +0000

    2018-03 part 2

--- 2018-03.fsx
+++ 2018-03.fsx
@@ -6,31 +6,47 @@ let readFile (filePath:string) = seq {
 
 // Sample input: `#1 @ 527,351: 24x10`
 let parseDataLine (dataLine:string) = 
-    let dat = dataLine.Split [|'@'; ' '; ','; ':'; 'x'|]
-    (dat.[3] |> int, 
+    let dat = dataLine.Split [|'#'; ' '; ','; ':'; 'x'|]
+    (dat.[1] |> int,
+     dat.[3] |> int, 
      dat.[4] |> int, 
      dat.[6] |> int, 
      dat.[7] |> int)
 
-let topLeftCornerAndRectangleSizeToCoordinates (left,  top,  width,  height) =
+let topLeftCornerAndRectangleSizeToCoordinates (i, left,  top,  width,  height) =
     seq {left..left+width-1} |> Seq.collect (fun x -> 
     seq {top..top+height-1}  |> Seq.map (fun y -> 
-    (x, y)))
+    (i, x, y)))
 
-let coordinatesToZeroPaddedFormat (x, y) = sprintf "%03i%03i" x y
+let coordinatesToZeroPaddedFormat (i, x, y) = (i, sprintf "%03i%03i" x y)
 
-let visitCoordinatesTrackVisits map coordinates =
-        let n = match Map.tryFind coordinates map with
-                | Some x -> x + 1
-                | None -> 1
-        Map.add coordinates n map        
+let visitCoordinatesTrackVisits (map, set) idxAndCoordinates =
+        let idx = fst idxAndCoordinates
+        let coords = snd idxAndCoordinates
+        let (updatedIndexOfFreeSquares, numClashes) = 
+                match Map.tryFind coords map with
+                | Some (i, x) ->
+                        let setOfFreeSquares = Set.add idx <| Set.add i set
+                        let numClashes = x + 1
+                        (setOfFreeSquares, numClashes)
+                | None -> (set, 1)
+        let updatedOverlapMap = Map.add coords (idx, numClashes) map
+        (updatedOverlapMap, updatedIndexOfFreeSquares)
 
 let greaterThanOneFolder state key value =
-        if value > 1 then state + 1 else state
+        if snd value > 1 then state + 1 else state
+let out = readFile "2018-03"
+        |> Seq.map parseDataLine
+        |> Seq.collect topLeftCornerAndRectangleSizeToCoordinates
+        |> Seq.map coordinatesToZeroPaddedFormat
+        |> Seq.fold visitCoordinatesTrackVisits (Map.empty, Set.empty)
 
-readFile "2018-03" 
-|> Seq.map parseDataLine
-|> Seq.collect topLeftCornerAndRectangleSizeToCoordinates
-|> Seq.map coordinatesToZeroPaddedFormat
-|> Seq.fold visitCoordinatesTrackVisits Map.empty
-|> Map.fold greaterThanOneFolder 0
+let overlappingInches =    
+        fst out |> Map.fold greaterThanOneFolder 0
+let oneAndOnlyFreeSquare = 
+        let items = snd out 
+        let all = Set.ofSeq [1..items.Count + 1]
+        Set.difference all items |> Set.minElement
+
+printfn "Part 1: %i" overlappingInches
+printfn "Part 2: %i" oneAndOnlyFreeSquare
