a0fcb45277353df204edd2db2988e2fe7c171a5d
Vasily <vasily.sliouniaev@jet.com>
2018-12-09 01:15:37 +0000

    Cleanup

--- 2018-04.fsx
+++ 2018-04.fsx
@@ -1,27 +1,33 @@
+// Parsing
 open System
 open System.Text.RegularExpressions
-open System.Collections.Generic
-
-type State = start = 0 | sleep = 1 | wake = 2
-type Event = {guard:int; time:DateTime; kind:State}
-let rex = new Regex(@"#(\d+) ", RegexOptions.Compiled)
-let parseGuardId s = (rex.Match s).Groups.[1].Value |> int
+type GuardState = start = 0 | sleep = 1 | wake = 2
+type Event = { guard:int; time:DateTime; kind:GuardState }
+let guardIdRex = new Regex(@"#(\d+) ", RegexOptions.Compiled)
+let parseGuardId s = (guardIdRex.Match s).Groups.[1].Value |> int
 let parseTime (s:string) = s.[1..16] |> DateTime.Parse
+type SleepInfo = { current:int; deepest:int; minute:int } 
+        with static member Default = { current=0; deepest=0; minute=0 }
+
+type SleepEvent = { minute:int; kind:GuardState }
+
+// If there is no guard id in the event, take the id from the previous event with one
+let parseEvent setGuardId (dataString:string) =
+        let kind = match dataString.[dataString.Length - 2] with
+                   | c when c = 'f' -> GuardState.start
+                   | c when c = 'e' -> GuardState.sleep
+                   | c when c = 'u' -> GuardState.wake
+                   | _ -> failwithf "Can't parse data: %s" dataString
+        let guard = if kind <> GuardState.start then setGuardId else parseGuardId dataString
+        { kind = kind; time = parseTime dataString; guard = guard }
+
+// Funcs
 let readFile (filePath:string) = seq {
     use sr = new System.IO.StreamReader (filePath)
     while not sr.EndOfStream do yield sr.ReadLine ()
 }
 
-let parseEvent lastGuard (s:string) =
-        let kind = match s.[s.Length - 2] with
-                   | c when c = 'f' -> State.start
-                   | c when c = 'e' -> State.sleep
-                   | c when c = 'u' -> State.wake
-                   | _ -> failwithf "Unknown string %s" s
-        let guard = if kind <> State.start then lastGuard else parseGuardId s
-        { kind = kind; time = parseTime s; guard = guard }
-
-let splitGuard list = 
+let splitEventsByGuards list = 
         let rec split (state:list<Event>) lastGuard list =
           match list with
           | [] -> state
@@ -31,36 +37,39 @@ let splitGuard list =
            // TODO: Make this tail-recursive
         split [] -1 list    
 
-let totalSleepReducer acc ((f:Event), (s:Event)) =
-        acc + match (f,s) with
-              | (f, s) when f.kind = State.sleep && s.kind = State.wake -> (s.time - f.time).TotalMinutes |> int
+let sleepDurationFromEventPair state ((first:Event), (second:Event)) =
+        state + match (first,second) with
+              | (f, s) 
+                when f.kind = GuardState.sleep && s.kind = GuardState.wake -> 
+                (s.time - f.time).TotalMinutes |> int
               | _ -> 0
 
-let guardSleptFor seq' =
-        Seq.pairwise seq' 
-        |> Seq.fold totalSleepReducer 0
-
-let data = readFile "2018-04" |> Seq.sort |> List.ofSeq |> splitGuard |> List.groupBy (fun ev -> ev.guard)
-
-let sleepiest data =
-        let mutable s = 0
-        let mutable maxSleep = 0
-        let mutable sleepiestMin = -1        
+let deepestSleep state data =
+                match data.kind with
+                | GuardState.sleep -> 
+                        let current = state.current + 1
+                        {               
+                            current = current
+                            deepest = if current > state.deepest then current else state.deepest
+                            minute  = if current > state.deepest then data.minute else state.minute 
+                        }
+                | GuardState.wake -> 
+                        { state with current = state.current - 1 }
+                | _ -> state
+let sleepiest data = 
         data 
-        |> Seq.map (fun ev -> (ev.time.Minute, ev.kind)) 
-        |> Seq.sort
-        |> Seq.iter (fun ev -> // TODO: Use a fold 
-                match snd ev with
-                | State.sleep -> 
-                        s <- s + 1
-                        if s > maxSleep then maxSleep <- s; sleepiestMin <- fst ev
-                | State.wake -> s <- s - 1
-                | _ -> ())
-        (maxSleep, sleepiestMin)               
+        |> Seq.map (fun ev -> { minute=ev.time.Minute; kind=ev.kind })
+        |> Seq.sortBy (fun ev -> ev.minute)
+        |> Seq.fold deepestSleep SleepInfo.Default 
+
+let guardSleptFor seq' = Seq.pairwise seq' |> Seq.fold sleepDurationFromEventPair 0
+
+// Solve
+let data = readFile "2018-04" |> Seq.sort |> List.ofSeq |> splitEventsByGuards |> List.groupBy (fun ev -> ev.guard)   
 let sleepiestGuard = data |> Seq.map (fun item -> fst item, (item |> snd |> guardSleptFor)) |> Seq.maxBy snd
-let sleepiestGuardData = data |> Seq.find (fun f -> fst sleepiestGuard = fst f) |> snd // This probably has a single function
-let part1 = sleepiestGuardData |> sleepiest |> snd |> (*) (fst sleepiestGuard) // 21956
+let sleepiestGuardData = data |> Seq.find (fun f -> fst sleepiestGuard = fst f) |> snd
+let part1 = sleepiestGuardData |> sleepiest |> (fun f -> f.minute ) |> (*) (fst sleepiestGuard) // 21956
 
 let part2 = data |> Seq.map (fun g -> 
         let depthAndMin = snd g |> sleepiest
-        (fst depthAndMin, fst g * (snd depthAndMin))) |> Seq.max |> snd // 124511
+        (depthAndMin.deepest, fst g, fst g * (depthAndMin.minute))) |> Seq.max // 134511
