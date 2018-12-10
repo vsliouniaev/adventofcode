5290f982bdb9cba5500d0f7ac8ec2e8fcf20ed56
Vasily <vasily.sliouniaev@jet.com>
2018-12-10 19:17:54 +0000

    2018-10

--- 2018-06.fsx
+++ 2018-06.fsx
@@ -7,8 +7,11 @@ let parseDataLine (dataLine:string) =
         |> Array.map int |> List.ofArray
 let dataPoints = readFile "2018-06" |> Seq.map parseDataLine |> List.ofSeq
 let maxDimension (points:list<list<'a>>) (d:int) = points |> Seq.map (fun i -> i.[d]) |> Seq.max
+let minDimension (points:list<list<'a>>) (d:int) = points |> Seq.map (fun i -> i.[d]) |> Seq.min
 let maxCoords (coords:list<list<'a>>) = [ for x in 0 .. coords.[0].Length - 1 -> maxDimension coords x ]
+let minCoords (coords:list<list<'a>>) = [ for x in 0 .. coords.[0].Length - 1 -> minDimension coords x ]
 let gridMax = maxCoords dataPoints
+let gridMin = minCoords dataPoints
 
 type PointDistance = { distance:int; idx:int; point:list<int>}
 let distance p = p.distance
@@ -16,31 +19,44 @@ let pointIdx p = p.idx
 let dist (a:list<int>) (b:list<int>) =
         Seq.fold2 (fun (distance,_) a' b' -> distance + abs(a' - b'), a) (0,[]) a b // Fold 2 sets of point coordinates into taxicab/manhattan distance
 
+
+// let sr = new System.IO.StreamWriter ("fail5")
+let mutable skipped = 0
 let closestTo (points:list<list<int>>) (currentPoint:list<int>) = 
-        points |> Seq.map (dist currentPoint)                                      // Calc distance to all data points for the current grid point 
-        |> Seq.mapi (fun idx (distance, point) -> 
-                { idx=idx; distance=distance; point=point })                       // Map to data structure
-        |> List.ofSeq
-        |> List.sortBy distance                                                    // Use list to leverage pattern-matching
-        |> (fun list -> 
-                match list with
-                | [f] -> Some f
-                | f::s when f.distance <> s.Head.distance -> Some f                // Only return data if it's the closest to exactly one coodrinate
-                | f::s when f.distance =  s.Head.distance -> None
-                | _ -> failwithf "Unknown list %A" list )
+        let p = points |> Seq.map (dist currentPoint)                                      // Calc distance to all data points for the current grid point 
+                |> Seq.mapi (fun idx (distance, point) -> 
+                        { idx=idx; distance=distance; point=point })                       // Map to data structure
+                |> List.ofSeq
+                |> List.sortBy distance                                                    // Use list to leverage pattern-matching
+        if p.[0].distance = p.[1].distance
+        then 
+                skipped <- skipped + 1
+                None
+        else
+                // if p.[0].idx = 'm' then sr.WriteLine p.[0].point
+                Some p.[0]
+        // |> (fun list -> 
+        //         match list with
+        //         | [f] -> Some f
+        //         | f::s when f.distance <> s.Head.distance -> Some f                // Only return data if it's the closest to exactly one coodrinate
+        //         | f::s when f.distance =  s.Head.distance -> None
+        //         | _ -> failwithf "Unknown list %A" list )
 
 // closestTo [[1;1];[1;6];[8;3];[3;4];[5;5];[8;9]] [5;2]
 
-[for x in 0..gridMax.[0] -> [for y in 0..gridMax.[1] -> [x; y]]] |> Seq.collect (fun x -> x)  // Create grid based on max coordinates of given points
+[for x in gridMin.[0]..gridMax.[0] -> [for y in gridMin.[1]..gridMax.[1] -> [x; y]]] |> Seq.collect id  // Create grid based on max coordinates of given points
 |> Seq.map (closestTo dataPoints) |> Seq.choose id                                            // At each point calc the closest data points
 |> Seq.groupBy pointIdx                                                                       // Group by the data point index  
 //|> List.ofSeq |> List.iter (fun s -> snd s |> Seq.iter (printfn "%A") )
-|> Seq.map (fun (idx, seq') -> (char (idx + 65), Seq.length seq', idx ))                      // Map index -> char length, data points -> their length 
+|> Seq.map (fun (idx, seq') -> (idx, Seq.length seq'))                      // Map index -> char length, data points -> their length 
 |> List.ofSeq
-|> Seq.sortByDescending (fun (_,x,_) -> x)
-|> List.ofSeq
+|> Seq.sortByDescending snd
+// |> Seq.sumBy snd
+|> List.ofSeq 
 |> List.head
 // |> (fun (_,_,s)-> s |> Seq.map (fun f -> f.point) |> Seq.iter (printfn "%A"))
 
 // 9899
-// b - 4011
+// b - 6717
+
+// int 'A'
---
+++ 2018-10.fsx
@@ -0,0 +1,63 @@
+// https://adventofcode.com/2018/day/9
+open System.Text.RegularExpressions
+
+let readFileLines (filePath:string) = seq {
+    use sr = new System.IO.StreamReader (filePath)
+    while not sr.EndOfStream do yield sr.ReadLine ()
+}
+
+let rex = new Regex(@"position=<\s?([-\d]+),\s+?([-\d]+)> velocity=<\s?([-\d]+),\s+?([-\d]+)", RegexOptions.Compiled)
+let parse s = 
+    let g = (rex.Match s).Groups
+    (g.[1].Value |> int,
+     g.[2].Value |> int,
+     g.[3].Value |> int,
+     g.[4].Value |> int)
+
+let maxX points = points |> Seq.map fst |> Seq.max
+let maxY points = points |> Seq.map snd |> Seq.max
+let minX points = points |> Seq.map fst |> Seq.min
+let minY points = points |> Seq.map snd |> Seq.min
+
+let gridSize points =
+    let size = maxX points - minX points * maxY points - minY points
+    size
+
+let getPoints data = data |> Seq.map (fun (x,y,_,_) -> (x,y)) 
+
+let printGrid data =
+    let points = getPoints data
+    let pthash = set points
+    [for y in minY points..maxY points ->
+        [for x in minX points..maxX points -> 
+            if Set.contains (x,y) pthash 
+            then printf "🌟"
+            else printf " " 
+        ] |> ignore
+        printfn ""
+    ]
+
+let moveby n (x,y,i,j) =
+    (x + (n * i), 
+     y + (n * j), 
+     i, j)
+
+let move d = d |> moveby 1
+
+let resetCoords data = 
+    let minx = data |> getPoints |> minX 
+    let miny = data |> getPoints |> minY
+    data |> Seq.map (fun (x,y,i,j) -> (x-minx, y-miny, i, j))
+
+let simulate jumpBy dat =
+    let rec doSim best minSize iterations dat =
+        let newPositions = dat |> Seq.map move |> resetCoords
+        let thisSize = newPositions |> getPoints |> gridSize
+        printfn "iteration: %i grid size %i" (iterations + jumpBy) minSize
+        match thisSize >= minSize with
+            | false -> newPositions |> doSim newPositions thisSize (iterations + 1)
+            | true -> best
+    
+    dat |> Seq.map (moveby jumpBy) |> resetCoords |> doSim [] 2147483647 0
+
+readFileLines "2018-10" |> Seq.map parse |> simulate 10000 |> printGrid
