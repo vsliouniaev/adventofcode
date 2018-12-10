313fdedad5d1b98e5989d997e5c5ab805f530cba
Vasily <vasily.sliouniaev@jet.com>
2018-12-10 21:43:35 +0000

    2018-06 part2

--- 2018-06.fsx
+++ 2018-06.fsx
@@ -5,13 +5,13 @@ let readFile (filePath:string) = seq {
 let parseDataLine (dataLine:string) =
         dataLine.Split [|','; ' '|] |> Array.filter (fun item -> item <> "")
         |> Array.map int |> List.ofArray
-let dataPoints = readFile "2018-06" |> Seq.map parseDataLine |> List.ofSeq
+let inputData = readFile "2018-06" |> Seq.map parseDataLine |> List.ofSeq
 let maxDimension (points:seq<list<'a>>) (d:int) = points |> Seq.map (fun i -> i.[d]) |> Seq.max
 let minDimension (points:seq<list<'a>>) (d:int) = points |> Seq.map (fun i -> i.[d]) |> Seq.min
 let maxCoords (coords:list<list<'a>>) = [ for x in 0 .. coords.[0].Length - 1 -> maxDimension coords x ]
 let minCoords (coords:list<list<'a>>) = [ for x in 0 .. coords.[0].Length - 1 -> minDimension coords x ]
-let gridMax = maxCoords dataPoints
-let gridMin = minCoords dataPoints
+let gridMax = maxCoords inputData
+let gridMin = minCoords inputData
 
 type PointDistance = { distance:int; idx:int; point:list<int>}
 let distance p = p.distance
@@ -36,10 +36,14 @@ let filterTouchingEdge (_,closestCoords) =
     let touches = closestCoords |> Seq.map (fun p -> p.point) |> Seq.exists (touchesGridEdge gridMax gridMin)
     touches |> not
 
-[for x in gridMin.[0]..gridMax.[0] -> [for y in gridMin.[1]..gridMax.[1] -> [x; y]]] |> Seq.collect id
-|> Seq.map (closestTo dataPoints) |> Seq.choose id
+let grid = [for x in gridMin.[0]..gridMax.[0] -> [for y in gridMin.[1]..gridMax.[1] -> [x; y]]] |> Seq.collect id |> List.ofSeq
+
+grid
+|> Seq.map (closestTo inputData) |> Seq.choose id
 |> Seq.groupBy pointIdx                           
 |> Seq.filter filterTouchingEdge          
 |> Seq.map (fun (idx, seq') -> (idx, Seq.length seq', seq'))
 |> Seq.sortByDescending (fun (_,x,_) -> x)
 |> List.ofSeq |> List.head
+
+grid |> List.filter (fun gridpoint -> (inputData |> List.map (fun f -> dist gridpoint f |> fst) |> List.sum) < 10000) |> List.length
