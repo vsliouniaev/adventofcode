f23c9e158670463110659a0a4dd43970512a266e
Vasily <vasily.sliouniaev@jet.com>
2018-12-11 08:14:40 +0000

    2018-11

--- 2018-07.2.fsx
+++ 2018-07.2.fsx
@@ -27,20 +27,20 @@ let prereq = raw |> toprereqDict
 let stepTime c = 
    (int c) - 4
 
-let rec doStep (availableSteps:list<char>) (completed:list<char>) (prereq:list<(char * list<char>)>) =
+let rec doStep (availableSteps:list<char>) (completed:list<char>) (prereq:list<(char * list<char>)>) totalTime =
     let sorted = availableSteps |> List.filter (fun i -> List.contains i completed |> not) |> List.sort 
-    let nextStep = sorted |> Seq.tryHead
     let longestTime = sorted |> List.tryLast
     printfn "%A" availableSteps.Length
-    match nextStep with
+    match longestTime with
     | None ->
         completed
     | Some c ->
+        let totalTime = totalTime + (stepTime c)
         let newPrereq = prereq |> List.map (fun (a,prereq) -> (a,prereq |> List.filter ((<>)c)))
         let unlockedSteps = newPrereq |> List.filter (fun (_,prereq) -> prereq = List.empty ) |> List.map fst
         let newPrereq = newPrereq |> List.filter (fun (_,prereq) -> prereq <> List.empty)
         let newCompleted = completed@[c]
         let newAvail = (availableSteps |> List.filter ((<>) c))@unlockedSteps
-        doStep newAvail newCompleted newPrereq
+        doStep newAvail newCompleted newPrereq totalTime
     
 new string [|for c in doStep firstStep [] prereq -> c|]
